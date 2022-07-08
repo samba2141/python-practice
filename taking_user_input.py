7717d0f10a0239f614201361e8c55af326b247e6
samba2141 <107167681+samba2141@users.noreply.github.com>
2022-07-08 22:12:28 +0530

    Add files via upload

---
+++ taking_user_input.py
@@ -0,0 +1,27 @@
+# a=2 ; b=4
+# print(a+b)
+
+# a=input()
+# b=input()
+# c=input()
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(a+b+c)
+
+
+# x=int(input())
+# y=int(input())
+# z=int(input())
+# print(x+y+z)
+
+p=float(input())
+q=float(input())
+r=float(input())
+print(p*q*r)
+print(p+q+r)
+print(p-q-r)
+print(p/q/r)
+
+# x,y=[float(r)for r in input().split()]
+# print(x+y+)
